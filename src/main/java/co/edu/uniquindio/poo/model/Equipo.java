acff8f5d3fe30b3b58e7d2c9286a6c504c5d13eb
Nicolas Arango Gutierrez <nicolas.arangog@uqvirtual.edu.co>
2025-10-28 13:32:00 -0500

    Se crea la clase Equipo con sus atributos

---
+++ src/main/java/co/edu/uniquindio/poo/model/Equipo.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.poo.model;
+
+import java.util.List;
+
+public class Equipo {
+    private String nombre;
+    private int puntajeTotal=0;
+    private UniVS ownedByUniVS;
+    private List<Jugador> listaJugadores=new LinkedList<Jugador>();
+
+}
